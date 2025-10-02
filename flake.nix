12f08238b10bd5d11a3bb4b1d5867be56bff9739
Luiz Ferraz <luiz@lferraz.com>
2025-10-02 00:36:42 -0300

    Configure environment for default plugins

--- flake.nix
+++ flake.nix
@@ -3,96 +3,136 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
-    fenix = {
-      url = "github:nix-community/fenix";
+    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1";
+
+    rust-overlay = {
+      url = "https://flakehub.com/f/oxalica/rust-overlay/0.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forEachSupportedSystem = f:
-      inputs.nixpkgs.lib.genAttrs supportedSystems (
-        system:
-          f {
-            pkgs = import inputs.nixpkgs {
-              inherit system;
-              overlays = [
-                inputs.self.overlays.default
-              ];
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+        inherit (pkgs) mkShell;
+        inherit (pkgs.lib) optionals makeLibraryPath;
+        inherit (pkgs.stdenv) isLinux isDarwin;
+
+        general-deps = with pkgs; [
+          # Rust
+          cargo-watch
+          cargo-expand
+          rust-analyzer-unwrapped
+          # Toml
+          taplo
+          # Other
+          vulkan-loader
+          vulkan-tools
+        ];
+
+        linux-deps = with pkgs; [
+          # Wayland
+          libxkbcommon
+          wayland.dev
+          # Xorg
+          xorg.libX11
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          # Other
+          openssl
+          pkg-config
+          alsa-lib.dev
+          systemd.dev
+          vulkan-validation-layers
+          clang
+          lld
+        ];
+
+        darwin-deps = with pkgs.darwin.apple_sdk.frameworks; [
+          AppKit
+          ApplicationServices
+          AudioToolbox
+          AudioUnit
+          Carbon
+          CoreAudio
+          CoreFoundation
+          CoreGraphics
+          CoreServices
+          CoreVideo
+          CoreMIDI
+          Foundation
+          IOKit
+          QuartzCore
+          Metal
+          OpenAL
+          Security
+          pkgs.libiconv
+        ];
+
+        web-deps = with pkgs; [
+          wasm-bindgen-cli
+          binaryen
+        ];
+
+        extensions = [
+          "clippy"
+          "rustfmt"
+          "rust-src"
+        ];
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells = {
+          # Regular shell
+          default = let
+            toolchain = pkgs.rust-bin.nightly.latest.default.override {
+              inherit extensions;
+              targets =
+                ["wasm32-unknown-unknown"]
+                ++ optionals isDarwin [
+                  "x86_64-apple-darwin"
+                  "aarch64-apple-darwin"
+                ];
             };
-          }
-      );
-  in {
-    overlays.default = _: prev: let
-      fenix = inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
-      toml = with builtins; (fromTOML (readFile ./rust-toolchain.toml)).toolchain;
-      toolchain = fenix.fromToolchainName {
-        name = toml.channel;
-        # sha256 = prev.lib.fakeSha256;
-        sha256 = "sha256-rCvHLpcrLKXtcpyysfi51zsSgMxB2+pXRIoJnUt2ORM=";
-      };
-    in {
-      # rustToolchain = toolchain."${toml.profile or "default"}Toolchain";
-      rustToolchain = fenix.combine (
-        [
-          # toolchain."${toml.profile or "default"}Toolchain"
-          (toolchain.withComponents (toml.components or []))
-        ]
-        ++ map (target: fenix.targets.${target}.${toml.channel}.rust-std) (toml.targets or [])
-      );
-    };
+            platform = pkgs.makeRustPlatform {inherit (toolchain) cargo rustc;};
+          in
+            mkShell rec {
+              buildInputs =
+                [
+                  toolchain
+                  platform.bindgenHook
+                ]
+                ++ general-deps
+                ++ web-deps
+                ++ optionals isLinux linux-deps
+                ++ optionals isDarwin darwin-deps;
 
-    formatter = forEachSupportedSystem ({pkgs}: pkgs.alejandra);
-
-    devShells = forEachSupportedSystem (
-      {pkgs}: {
-        default = pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            rustToolchain
-            clang
-            lld
-
-            # Wayland
-            libxkbcommon.dev
-            wayland.dev
-            # Xorg
-            xorg.libX11
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-            # Other
-            openssl
-            pkg-config
-            alsa-lib.dev
-            systemd.dev
-            vulkan-validation-layers
-
-            vulkan-loader
-            vulkan-tools
-
-            cargo-deny
-            cargo-edit
-            cargo-expand
-            cargo-watch
-            rust-analyzer
-          ];
-
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-            RUST_BACKTRACE = "1";
-            CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG = "true";
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          };
+              RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+              LD_LIBRARY_PATH = makeLibraryPath buildInputs;
+              RUSTFLAGS = "-Zshare-generics=y -Zthreads=0";
+            };
+          # web = let
+          #   toolchain = pkgs.rust-bin.nightly.latest.default.override {
+          #     targets = ["wasm32-unknown-unknown"];
+          #   };
+          # in
+          #   mkShell rec {
+          #     buildInputs = [toolchain] ++ general-deps ++ web-deps;
+          #
+          #     RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          #     LD_LIBRARY_PATH = makeLibraryPath buildInputs;
+          #     RUSTFLAGS = "-Zshare-generics=y -Zthreads=0";
+          #   };
         };
       }
     );
-  };
 }
