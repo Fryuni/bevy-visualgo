53429d542bb81825aa7b3db2cd92858c14f8da5a
Luiz Ferraz <luiz@lferraz.com>
2025-10-07 21:01:27 -0300

    Add bevy_ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,35 @@
-use bevy::prelude::*;
+use bevy::{app::AppExit, prelude::*};
+use bevy_ratatui::event::KeyEvent;
+use bevy_ratatui::{RatatuiContext, RatatuiPlugins};
+use ratatui::crossterm::event::KeyCode;
+use ratatui::text::Text;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_plugins((
+            MinimalPlugins.set(bevy::app::ScheduleRunnerPlugin::run_loop(
+                std::time::Duration::from_secs_f32(1. / 60.),
+            )),
+            RatatuiPlugins::default(),
+        ))
+        .add_systems(PreUpdate, input_system)
+        .add_systems(Update, draw_system)
         .run();
 }
 
-pub struct HelloPlugin;
+fn draw_system(mut context: ResMut<RatatuiContext>) -> Result {
+    context.draw(|frame| {
+        let text = Text::raw("hello world\npress 'q' to quit");
+        frame.render_widget(text, frame.area());
+    })?;
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
-    }
+    Ok(())
 }
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
+fn input_system(mut events: EventReader<KeyEvent>, mut exit: EventWriter<AppExit>) {
+    for event in events.read() {
+        if let KeyCode::Char('q') = event.code {
+            exit.write_default();
         }
     }
 }
